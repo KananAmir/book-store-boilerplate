a5f318356c5c559f0437d3244fff8ea38e92c2d8
KananAmir <amirovknn@gmail.com>
2024-12-06 21:05:22 +0400

    boilerplate

---
+++ src/constants/api.js
@@ -0,0 +1,11 @@
+const BASE_URL = "http://localhost:8080";
+
+const endpoints = {
+  books: "/api/books",
+  genres: "/api/genres",
+  users: "/api/users",
+  orders: "/api/orders",
+};
+
+export { endpoints };
+export default BASE_URL;
